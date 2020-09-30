87bda99755c7d034a7bb0e66369e0458ae2f4341
Hannah Arnett <hannahtesaker@gmail.com>
2020-09-30 10:34:58 -0700

    Generate shopping list from recipe list

--- src/List.js
+++ src/List.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import RecipeCard from "./RecipeCard";
 
 const List = (props) => {
+  const [shoppingList, setShoppingList] = useState([]);
   const { list, deleteList, lists } = props;
+  console.log(list);
   const deleteHandler = () => {
     deleteList(list.id);
     props.history.push("/");
   };
+  const generateShoppingList = () => {
+    let ingredients = [];
+    list.recipes.map((recipe) => {
+      ingredients.push(...recipe.ingredients);
+    });
+
+    setShoppingList(ingredients);
+  };
   return (
     <div>
-      <h1>{list.title}</h1>
+      <h2>{list.title}</h2>
       <button onClick={deleteHandler}>Delete List</button>
       {list.recipes.length > 0 ? (
-        list.recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} lists={lists} />
-        ))
+        <div>
+          {list.recipes.map((recipe) => (
+            <RecipeCard key={recipe.id} recipe={recipe} lists={lists} />
+          ))}
+          <button onClick={generateShoppingList}>Generate shopping list</button>
+        </div>
       ) : (
         <div>
           <h2>No recipes yet!</h2>
           <Link to="/recipes">Browse recipes</Link>
+          <Link to="/recipe/new">Add new recipe</Link>
         </div>
       )}
+      {shoppingList &&
+        shoppingList.map(({ name, qty, unit }, index) => (
+          <p key={index}>{`${qty} ${unit} ${name}`}</p>
+        ))}
     </div>
   );
 };
