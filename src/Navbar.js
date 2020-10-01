5faeb8c3d4b82a2d7c322f12588044d185f8b1a1
Hannah Arnett <hannahtesaker@gmail.com>
2020-10-01 09:22:55 -0700

    Add sass and navbar styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import "./navbar.scss";
+
 const Navbar = () => {
   return (
     <nav>
+      <h1>weekly</h1>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/recipes">Recipes</NavLink>
     </nav>
