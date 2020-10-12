d0e4d068cb06b69b9a524c799f4dfab994f2f901
Hannah Arnett <hannahtesaker@gmail.com>
2020-10-12 16:17:42 -0700

    Add PageHeader

--- src/Lists.js
+++ src/Lists.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PageHeader from "./PageHeader";
 
 import "./lists.scss";
 
 const Lists = ({ lists }) => {
   const miniLists = lists.map((list) => {
     return (
-      <li className="">
+      <li key="list.id">
         <Link to={`/list/${list.id}`}>
           <h2 className="list-title">{list.title}</h2>
         </Link>
@@ -16,7 +17,17 @@ const Lists = ({ lists }) => {
   });
   return (
     <div className="all-lists-container">
-      <h1 className="all-lists-title visually-hidden">All lists</h1>
+      <PageHeader>
+        <h1 className="page-header-title">All lists</h1>
+        <div className="btn-wrap">
+          <Link to="/list/new">
+            <button className="list-btn">New List</button>
+          </Link>
+          <Link to="/recipe/new">
+            <button className="list-btn">New Recipe</button>
+          </Link>
+        </div>
+      </PageHeader>
       <ul className="all-lists-ul">{miniLists}</ul>
     </div>
   );
