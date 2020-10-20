668da39f9930dd0a198606697cfd9797f2703f46
Hannah Arnett <hannahtesaker@gmail.com>
2020-10-20 10:23:42 -0700

    Fix tabbing bug in burger navgation

--- src/components/navbar/navItems/NavItems.js
+++ src/components/navbar/navItems/NavItems.js
@@ -4,15 +4,19 @@ import { NavLink } from "react-router-dom";
 import "./navItems.scss";
 
 const NavItems = ({ open, onClick }) => {
+  const handleClick = () => {
+    // only for mobile navigation
+    open && onClick();
+  };
   return (
     <ul className={`burger-ul ${open ? "open" : null}`}>
       <li className="nav-link">
-        <NavLink to="/lists" onClick={onClick}>
+        <NavLink to="/lists" onClick={handleClick}>
           Lists
         </NavLink>
       </li>
       <li className="nav-link">
-        <NavLink to="/recipes" onClick={onClick}>
+        <NavLink to="/recipes" onClick={handleClick}>
           Recipes
         </NavLink>
       </li>
